6447b840c423d244209520f6d2161ced2915de30
Usamaniaz219 <usamaniaz219@gmail.com>
2025-03-13 08:34:27 -0700

    requirements.txt file is added

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+with open("README.md","r",encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Chiken_disease_classification"
+AUTHOR_USER_NAME = "USAMA AHMAD"
+SRC_REPO = "cnnClassifier"
+AUTHOR_EMAIL = "ahmadusama34@gmail.com"
+
+  
