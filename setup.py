c5ebf7c36c3b6e9cfb4aef89237a6d67be805773
Usamaniaz219 <usamaniaz219@gmail.com>
2025-03-20 10:42:36 -0700

    model protype is created in jupyter notebook

--- setup.py
+++ setup.py
@@ -10,4 +10,24 @@ AUTHOR_USER_NAME = "USAMA AHMAD"
 SRC_REPO = "cnnClassifier"
 AUTHOR_EMAIL = "ahmadusama34@gmail.com"
 
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for CNN app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+
+
+)
+
+
+
   
